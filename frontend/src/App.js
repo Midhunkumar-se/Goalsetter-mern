6b60ecceb2b733d0e50a7eee046b85965d34ec39
Midhunkumar-se <midhu025@gmail.com>
2023-04-30 14:58:52 +0530

    Added frontend UI

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import RootLayout from "./pages/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
